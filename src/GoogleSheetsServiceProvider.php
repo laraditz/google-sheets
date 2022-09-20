4db9e41d2d1efd0fa67deed55949f453454afe0a
Farhan <raditzfarhan@gmail.com>
2022-09-20 16:58:01 +0800

    Update Google Sheet class
    Add method to get the sheets data
    Update config file
    Update readme

--- src/GoogleSheetsServiceProvider.php
+++ src/GoogleSheetsServiceProvider.php
@@ -10,37 +10,11 @@ class GoogleSheetsServiceProvider extends ServiceProvider
      * Bootstrap the application services.
      */
     public function boot()
-    {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'google-sheets');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'google-sheets');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
+    {     
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('google-sheets.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/google-sheets'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/google-sheets'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/google-sheets'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            ], 'config');         
         }
     }
 
